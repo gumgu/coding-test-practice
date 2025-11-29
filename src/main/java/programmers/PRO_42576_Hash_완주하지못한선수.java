bc084ba6978c635c05f5c9638d1a9d17db3a4404
gumgu <bienlee@naver.com>
2025-11-29 14:29:21 +0900

    refactor: 코드 성능 개선

--- src/main/java/programmers/PRO_42576_Hash_완주하지못한선수.java
+++ src/main/java/programmers/PRO_42576_Hash_완주하지못한선수.java
@@ -1,26 +1,27 @@
 package main.java.programmers;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class PRO_42576_Hash_완주하지못한선수 {
     public String solution(String[] participant, String[] completion) {
-        // 참가자 순환하며 참여 목록 Map 에 저장
-        HashMap<String, Integer> participants =     new HashMap<>();
-        for (String parti : participant) {
-            participants.put(parti, participants.getOrDefault(parti, 0) + 1);
+        // 인터페이스 사용 (Map)
+        Map<String ,Integer> map = new HashMap<>();
+
+        // 참가자 카운팅
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
         }
 
-        // 완료자 순환하며 참가자 count--;
-        for (String comple : completion) {
-            participants.put(comple, participants.get(comple) - 1);
+        // 완주자 차감
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
         }
 
-        // Map의 value중 1인 데이터를 반환
-        for (String key : participants.keySet()) {
-            Integer value = participants.get(key);
-
-            if (value == 1) {
-                return key;
+        // 결과 반환: entrySet()을 사용하여 재조회 비용 절감
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 0) { // 1인 경우 혹은 그 이상
+                return entry.getKey();
             }
         }
 
