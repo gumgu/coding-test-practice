0a991cdade241deb4d5c2a6117d71c08c7e8a8c0
gumgu <bienlee@naver.com>
2026-01-17 12:54:58 +0900

    style: [BOJ] 평균 (1546)
    - 코드 스타일 개선

--- src/main/java/boj/BOJ_1546_평균.java
+++ src/main/java/boj/BOJ_1546_평균.java
@@ -3,43 +3,28 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.StringTokenizer;
 
 public class BOJ_1546_평균 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine()); // 과목 개수 입력
-        String numbers = br.readLine(); // 한 줄을 통째로 문자열로 읽음
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()); // 한 줄을 통째로 문자열로 읽음
 
-        String[] strNumbers = numbers.split(" ");
+        double max = 0L;
+        double sum = 0L;
 
-        // 1. 파라미터를 list로 변환
-        List<Integer> intNumbers = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            intNumbers.add(Integer.parseInt(strNumbers[i]));
+            double num = Double.parseDouble(st.nextToken());
+
+            sum += num;      // 리스트에 넣지 않고 바로 합계 누적
+            if (max < num) { // 정렬 대신 비교로 최댓값 찾기 (O(N))
+                max = num;
+            }
         }
 
-        // 2. 정렬해서 최대값 구하기
-        Collections.sort(intNumbers, Collections.reverseOrder());
-        int max = intNumbers.get(0);
-
-        // 3. 각 점수 가공
-        List<Double> adjustedNumbers = new ArrayList<>();
-        for (int i =0; i <n; i++) {
-            adjustedNumbers.add(intNumbers.get(i) / (double) max + 100);
-        }
-
-        // 4. 평균 구하기
-        double sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum += adjustedNumbers.get(i);
-        }
-        double result = sum / adjustedNumbers.size();
-
-        // 5. 정답 출력
+        double result = (sum / max * 100)/n;
         System.out.println(result);
     }
 }
